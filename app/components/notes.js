5fff768c2b58676e24b77721d5830506e6731152
Binil B <binil.b.re@gmail.com>
2024-05-26 20:07:57 +0530

    Note Completion

--- app/components/notes.js
+++ app/components/notes.js
@@ -1,10 +1,19 @@
 "use client";
 import { useState } from "react";
+import db from "../appwrite/database";
 export default function Note({ noteData }) {
   const [note, setNote] = useState(noteData);
+
+  const handleUpdate = async () => {
+    const completed = !note.completed;
+    db.tasks.update(note.$id, { completed });
+    setNote({ ...note, completed: completed });
+  };
   return (
     <main>
-      <div>{note.completed ? <strike>{note.body}</strike> : note.body}</div>
+      <div onClick={handleUpdate}>
+        {note.completed ? <strike>{note.body}</strike> : note.body}
+      </div>
     </main>
   );
 }
