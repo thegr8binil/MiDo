e9189ef6f06d30913f0f81ae8a0f76d4076c3e44
Binil B <binil.b.re@gmail.com>
2024-05-26 21:15:35 +0530

    Styles

--- app/components/notes.js
+++ app/components/notes.js
@@ -18,8 +18,8 @@ export default function Note({ setNotes, noteData }) {
   }
 
   return (
-    <main className="flex justify-between px-2 py-3 mb-4 rounded-lg bg-slate-200">
-      <div onClick={handleUpdate}>
+    <main className="flex justify-between px-2 py-3 mb-4 rounded-lg shadow-md bg-slate-100">
+      <div onClick={handleUpdate} className="w-full">
         {note.completed ? <strike>{note.body}</strike> : note.body}
       </div>
       <div onClick={handleDelete} className="flex items-center justify-center bg-blue-200 rounded-full w-7 h-7">
